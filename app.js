7ce91b51b81c9c897434ec7918750e17fe74becf
Dibakar Debnath <55661515+dibakart4@users.noreply.github.com>
2022-05-30 23:26:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');                //m
+const bodyParser = require('body-parser');         //mw
+const ejs = require('ejs');                        //h
+const Nexmo = require('nexmo');                    //Rst
+const socketio = require('socket.io');              //js-L
+
+
+// Init Nexmo
+const nexmo = new Nexmo({
+  apiKey: '6d1866c7',
+  apiSecret: 'nvUVl9F2nzwQOvZK'
+}, { debug: true });
+
+// Init app
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Catch form submit
+app.post('/', (req, res) => {
+  // res.send(req.body);
+  // console.log(req.body);
+  const { number, text } = req.body;
+
+  nexmo.message.sendSms(
+    '+918837215173', number, text, { type: 'unicode' },
+    (err, responseData) => {
+      if(err) {
+        console.log(err);
+      } else {
+        const { messages } = responseData;
+        const { ['message-id']: id, ['to']: number, ['error-text']: error  } = messages[0];
+        console.dir(responseData);
+        // Get data from response
+        const data = {
+          id,
+          number,
+          error
+        };
+
+        // Emit to the client
+        io.emit('smsStatus', data);
+      }
+    }
+  );
+});
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+  console.log('Connected');
+  io.on('disconnect', () => {
+    console.log('Disconnected');
+  })
+});
